6d088c8caaebcf63e0a0a61d0f7da6ebe8eccb47
saryusirohi30 <saryu@targeticon.com>
2024-07-01 19:29:08 +0530

    [latest]

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 
 // image import
 import Logo from "@/assets/images/logo/logo.png";
-import Illustration from "@/assets/images/auth/ils1.svg";
+import Illustration from "@/assets/images/login-img.png";
 
 const login = () => {
   return (
@@ -14,15 +14,16 @@ const login = () => {
       <div className="loginwrapper">
         <div className="lg-inner-column">
           <div className="left-column relative z-[1]">
-            <div className="max-w-[520px] pt-20 ltr:pl-20 rtl:pr-20">
+            <div className="max-w-[600px] pt-20 ltr:pl-20 rtl:pr-20">
               <Link to="/">
                 <img src={Logo} alt="" className="mb-10" width={200} />
               </Link>
               <h4>
-                Unlock your Project
-                <span className="text-slate-800 dark:text-slate-400 font-bold">
-                  performance
+              <span className="text-scooton-800 dark:text-slate-400 font-bold">
+                Parcel delivery, made easy  <br></br>
                 </span>
+                Realtime parcel delivery service in Your city
+                
               </h4>
             </div>
             <div className="absolute left-0 2xl:bottom-[-160px] bottom-[-130px] h-full w-full z-[-1]">
