0b711506245d7f87c7d2919f04527f82e45197e4
nimeesha1 <nimeesha@targeticon.com>
2024-11-20 10:38:28 +0530

    [cancel icon]

--- src/pages/orders/all-orders.jsx
+++ src/pages/orders/all-orders.jsx
@@ -110,7 +110,7 @@ const COLUMNS = (openIsNotificationModel, openIsDeleteOrder, ordersType) => [
     Header: "Delivery Address",
     accessor: "orderHistory.deliveryAddressDetails.addressLine1"
   },
-  ...(ordersType === "PLACED"
+  ...(ordersType === "PLACED" 
     ? [
       {
         Header: "Notification",
@@ -127,19 +127,38 @@ const COLUMNS = (openIsNotificationModel, openIsDeleteOrder, ordersType) => [
       },
     ]
     : []),
-  {
-    Header: "Cancel",
-    accessor: "",
-    Cell: (row) => {
-      return (
-        <div onClick={() => openIsDeleteOrder(row.row.original.order_Id)}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243" /></svg>
 
-        </div>
-      )
+  ...(ordersType === "PLACED"  || ordersType === "ACCEPTED"
+    ? [
+      {
+        Header: "Cancel",
+        accessor: "",
+        Cell: (row) => {
+          return (
+            <div onClick={() => openIsDeleteOrder(row.row.original.order_Id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243" /></svg>
+    
+            </div>
+          )
+    
+        }
+      },
+    ]
+    : []),
 
-    }
-  },
+  // {
+  //   Header: "Cancel",
+  //   accessor: "",
+  //   Cell: (row) => {
+  //     return (
+  //       <div onClick={() => openIsDeleteOrder(row.row.original.order_Id)}>
+  //         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243" /></svg>
+
+  //       </div>
+  //     )
+
+  //   }
+  // },
   {
     Header: "Vehicle Type",
     accessor: "vehicleId",
