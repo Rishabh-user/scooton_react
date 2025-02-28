3cf5773e8d3bbaf4b11b50566cd1a20b5d0032ec
Rishabh-user <rishabh@targetico.com>
2025-02-28 17:26:56 +0530

    [noti]

---
+++ src/requestFCMToken.js
@@ -0,0 +1,45 @@
+import { messaging, getToken } from "./firebaseConfig";
+
+const requestFCMToken = async (userId) => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: "BLZ7kya0uWE7w3PN1zgT3JG_swrSdWfX5g5PAaXhuAXXCldFtGMkv2TDDcmB7ESCv3A7IpW9W-wSpTBJJ76mHCY", // Replace with your VAPID Key
+      });
+
+      if (token) {
+        console.log("FCM Token:", token);
+        // Send the token to the backend
+        await sendTokenToServer(userId, token);
+      } else {
+        console.log("No FCM token available");
+      }
+    } else {
+      console.log("Notification permission denied");
+    }
+  } catch (error) {
+    console.error("Error getting FCM Token:", error);
+  }
+};
+
+// Send the token to your backend
+const sendTokenToServer = async (userId, token) => {
+  try {
+    await fetch(
+      `https://scooton-api-dev.el.r.appspot.com/notification/admin/${userId}/update-fcm`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cmToken: token }),
+      }
+    );
+    console.log("FCM Token saved for user:", userId);
+  } catch (error) {
+    console.error("Error saving FCM Token:", error);
+  }
+};
+
+export default requestFCMToken;
