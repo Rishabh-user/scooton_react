cd1110c28e2727f602ef8b0b62d88b9e55c7996f
nimeesha1 <nimeesha@targeticon.com>
2025-03-10 18:22:55 +0530

    [css]

--- src/pages/orders/order-detail.jsx
+++ src/pages/orders/order-detail.jsx
@@ -517,19 +517,19 @@ const OrderDetail = () => {
                         <table className="w-full border-collapse table-fixed dark:border-slate-700 dark:border">
                             <tbody>
                                 <tr className="border-b border-slate-100 dark:border-slate-700">
-                                    <td className="text-slate-900 dark:text-slate-300 text-sm  font-normal ltr:text-left ltr:last:text-right rtl:text-right rtl:last:text-left px-6 py-2">
+                                    <td className="px-6 py-2">
                                         Order Id
                                     </td>
-                                    <td className="text-slate-900 dark:text-slate-300 text-sm  font-normal ltr:text-left ltr:last:text-right rtl:text-right rtl:last:text-left px-6 py-2">
+                                    <td className="px-6 py-2">
                                      
                                         {thirdPartyUsername ? orderDetails.orderId : orderDetails.order_Id}
                                     </td>
                                 </tr>
                                 <tr className="border-b border-slate-100 dark:border-slate-700">
-                                    <td className="text-slate-900 dark:text-slate-300 text-sm  font-normal ltr:text-left ltr:last:text-right rtl:text-right rtl:last:text-left px-6 py-2">
+                                    <td className="px-6 py-2">
                                         Order Date
                                     </td>
-                                    <td className="text-slate-900 dark:text-slate-300 text-sm  font-normal ltr:text-left ltr:last:text-right rtl:text-right rtl:last:text-left px-6 py-2">
+                                    <td className="px-6 py-2">
                                         {thirdPartyUsername ? new Date(orderDetails.orderDateTime).toLocaleString("en-US", {
                                             year: "numeric",
                                             month: "short",
